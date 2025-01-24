5b8b1be4d063b7f6a2c7eeb7a29a58d990597ff2
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2025-01-24 15:09:35 -0700

    adds code from different file

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo()
+{
+    return (
+        <div>
+            <h1>Daniel Chu</h1>
+            <p>
+                <Link href="https://github.com/HamDan36/cprg306-assignments">HamDan36</Link>
+            </p>
+        </div>
+    );
+}
